82899118a8aff4b7041a6f4483e1420e4df43f6e
AnjaniGourisaria <82677043+AnjaniGourisaria@users.noreply.github.com>
2021-11-23 02:29:38 +0530

    done py setup and profile

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render ,HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+    #return HttpResponse("this is done suessfully")
+def fashion(request):
+    return render(request,'fashion.html')
+def jewellery(request):
+    return render(request,'jewellery.html')
+def electronic(request):
+    return render(request,'electronic.html')
+def addcart(request):
+    #return HttpResponse("this is done suessfully")
+    return render(request,'addcart.html')
+def profile(request):
+    #return HttpResponse("this is done suessfully")
+    return render(request,'profile.html')
+def search(request):
+    #return HttpResponse("this is done suessfully")
+    return render(request,'profile.html')
