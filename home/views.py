3065a18d38145379cdde1400c60b5f6a978185c2
AnjaniGourisaria <82677043+AnjaniGourisaria@users.noreply.github.com>
2021-11-30 00:23:49 +0530

    logn and regestrarion done

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.http import HttpRequest
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
 #from .models import Feature
 
 # Create your views here.
@@ -28,8 +30,6 @@ def addcart(request):
     # return HttpResponse("this is done suessfully")
     return render(request, 'addcart.html')
 
-def sign_in(request):
-    return render(request,'sign_in.html')
 
 def sign_up(request):
         if request.method == 'POST':
@@ -37,14 +37,48 @@ def sign_up(request):
             email = request.POST['email']
             password = request.POST['password']
             reppassword = request.POST['reppassword']
-            print("usernmae",username)
-            print("enail",email)
+            print("username",username)
+            print("email",email)
             print("password",password)
             print("repassword",reppassword)
-            
+            if password == reppassword:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request ,"Email Exist in database")
+                    return redirect('sign_up')
+                elif len(username)>5 and User.objects.filter(username=username).exists():
+                    messages.error(request ,"Username Exist in database")
+                    return redirect('sign_up')
+                elif len(password)<8:
+                     messages.error(request ,"bigger password near 8+")
+                     return redirect('sign_up')
+                else:
+                    user = User.objects.create_user(username=username,email=email,password=password)
+                    user.save();
+                    return redirect('login')       
+            else:
+                messages.error(request ,"Password not matched")
+                return redirect('sign_up')
         else:
             return render(request, 'sign_up.html')
             
+def sign_in(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        print(username)
+        print(password)
+        user = authenticate(request, username=username,password=password)
+        
+        if user is not None:
+            print('logined')
+            auth_login(request, user)
+            messages.success(request,'Login Sucessfully')
+            return redirect('/')
+        else:
+            messages.error(request,'Invailed credentials')
+            return redirect('sign_in')
+    else:
+        return render(request,'sign_in.html')
 
     
  # return HttpResponse("this is done suessfully")
